8df51f0223f75d028d5af2ce863d69cc8209ce59
Nicolas Gonzalez <nicolas.gonzalez.cl@gmail.com>
2023-01-13 22:55:43 -0300

    a lot of work

--- Makefile
+++ Makefile
@@ -4,3 +4,4 @@
 # init django app
 # docker-compose run --rm app sh -c "django-admin startproject app ."
 
+docker-compose run --rm app sh -c "python manage.py startapp core"
---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+def substract(x, y):
+    """Susbtract x from y return result"""
+    return y - x
