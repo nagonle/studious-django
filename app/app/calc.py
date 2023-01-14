f93cd488fb180762c52faa662a45dac74aaf8b6a
Nicolas Gonzalez <nicolas.gonzalez.cl@gmail.com>
2023-01-14 20:17:02 -0300

    flake8 fix

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,7 @@ def add(x, y):
     """Add x and y and return result"""
     return x + y
 
+
 def substract(x, y):
     """Susbtract x from y return result"""
     return y - x
--- app/test.py
+++ app/test.py
@@ -10,7 +10,6 @@ class CalcTests(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
-
     def test_substract_numbers(self):
         """Test substracting numbers"""
         res = calc.substract(10, 15)
